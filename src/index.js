cbf6845a47756818599732b2e313ccfaa74cbb73
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2021-10-11 11:40:04 +0300

    Add React Context to the project

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { RoomProvider } from './context';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
+  <RoomProvider>
+    <React.StrictMode>
+      <Router>
+        <App />
+      </Router>
+    </React.StrictMode>
+  </RoomProvider>,
   document.getElementById('root')
 );
 
