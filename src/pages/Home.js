9267f6e4c9811a90a1d1465a48318d6237cb0c7f
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2021-10-11 11:41:02 +0300

    Add Room Component and functionality.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,7 @@ import Banner from '../components/Banner';
 import Hero from '../components/Hero';
 import { Link } from 'react-router-dom';
 import Services from '../components/Services';
+import FeaturedRooms from '../components/FeaturedRooms';
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
         </Banner>
       </Hero>
       <Services />
+      <FeaturedRooms />
     </>
   );
 };
