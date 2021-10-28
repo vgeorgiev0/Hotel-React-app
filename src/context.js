6522a8266fa08b90bb37cf17e4f2aa8060f688dc
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2021-10-28 17:29:32 +0300

    Add Changes to the Context and Add RoomList Component

--- src/context.js
+++ src/context.js
@@ -9,6 +9,15 @@ export default class RoomProvider extends Component {
     sortedRooms: [],
     featuredRooms: [],
     loading: true,
+    type: 'all',
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   //   getData function
@@ -17,11 +26,16 @@ export default class RoomProvider extends Component {
     // this.getData
     let rooms = this.formatData(items);
     let featuredRooms = rooms.filter((room) => room.featured === true);
+    let maxPrice = Math.max(...rooms.map((item) => item.price));
+    let maxSize = Math.max(...rooms.map((item) => item.size));
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
       loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   }
 
@@ -42,9 +56,26 @@ export default class RoomProvider extends Component {
     return room;
   };
 
+  handleChange = (event) => {
+    const type = event.target.type;
+    const name = event.target.name;
+    const value = event.target.value;
+    console.log(type, name, value);
+  };
+
+  filterRooms = () => {
+    console.log('hi');
+  };
+
   render() {
     return (
-      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
+      <RoomContext.Provider
+        value={{
+          ...this.state,
+          getRoom: this.getRoom,
+          handleChange: this.handleChange,
+        }}
+      >
         {this.props.children}
       </RoomContext.Provider>
     );
@@ -52,5 +83,15 @@ export default class RoomProvider extends Component {
 }
 
 const RoomConsumer = RoomContext.Consumer;
+
+export function withRoomConsumer(Component) {
+  return function ConsumerWrapper(props) {
+    return (
+      <RoomConsumer>
+        {(value) => <Component {...props} context={value} />}
+      </RoomConsumer>
+    );
+  };
+}
 
 export { RoomProvider, RoomConsumer, RoomContext };
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,15 +2,20 @@ import React from 'react';
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
+import RoomContainer from '../components/RoomContainer';
+
 const Rooms = () => {
   return (
-    <Hero hero='roomsHero'>
-      <Banner title='Our rooms'>
-        <Link className='btn-primary' to='/'>
-          Return Home
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero hero='roomsHero'>
+        <Banner title='Our rooms'>
+          <Link className='btn-primary' to='/'>
+            Return Home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomContainer />
+    </>
   );
 };
 
