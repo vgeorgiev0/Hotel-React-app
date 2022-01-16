8aa1fa87577399ad913d3ee5928217dc6944f32d
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2022-01-16 08:54:41 +0200

    Fix Paralax Width

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -15,12 +15,14 @@ const Restaurant = () => {
   );
   return (
     <div>
-      <Parallax bgImage={mainImage} strength={100}>
-        <Banner title={mainTitle} subtitle={mainSubtitle}>
-          <Link to='/contact' className='btn-primary'>
-            <Trans i18nKey='about'>Contact Us</Trans>
-          </Link>
-        </Banner>
+      <Parallax bgImage={mainImage} strength={500}>
+        <div className='para'>
+          <Banner title={mainTitle} subtitle={mainSubtitle}>
+            <Link to='/contact' className='btn-primary'>
+              <Trans i18nKey='about'>Contact Us</Trans>
+            </Link>
+          </Banner>
+        </div>
       </Parallax>
       <div>
         <Hero></Hero>
@@ -30,3 +32,21 @@ const Restaurant = () => {
 };
 
 export default Restaurant;
+
+// <Parallax
+//   bgImage='/path/to/another/image'
+//   renderLayer={(percentage) => (
+//     <div
+//       style={{
+//         position: 'absolute',
+//         background: `rgba(255, 125, 0, ${percentage * 1})`,
+//         left: '50%',
+//         top: '50%',
+//         width: percentage * 500,
+//         height: percentage * 500,
+//       }}
+//     />
+//   )}
+// >
+//   <p>... Content</p>
+// </Parallax>;
