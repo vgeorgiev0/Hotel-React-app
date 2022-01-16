08f3e9a732aa0e5055b9e03a3d005b56c02036c6
vgeorgiev0 <valentingeorgiev1996@gmail.com>
2022-01-16 15:30:29 +0200

    Add Basic Services Logic to Restaurant Page

--- src/pages/Restaurant.js
+++ src/pages/Restaurant.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Banner from '../components/Banner';
 import Hero from '../components/Hero';
+import Title from '../components/Title';
+import ImageGallery from '../components/ImageGallery';
 import { Parallax } from 'react-parallax';
 import { Link } from 'react-router-dom';
 import { Trans } from 'react-i18next';
 import mainImage from '../images/contact.jpg';
-import ImageGallery from '../components/ImageGallery';
 
 const Restaurant = () => {
   const mainTitle = (
@@ -25,6 +26,9 @@ const Restaurant = () => {
           </Banner>
         </div>
       </Parallax>
+      <div className='services'>
+        <Title title={'Have a celebration with us!'} />
+      </div>
       <div>
         <ImageGallery />
       </div>
