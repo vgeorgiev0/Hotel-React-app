d0c10af8bf9fdb5906af6f00f324ebb9f3024316
VGeorgiev <vgeorgiev@up2technology.com>
2022-06-21 15:45:58 +0300

    BREAKING CHANGE: Update dependencies

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import loadingGif from '../images/gif/loading-6.gif';
 
 const Loading = () => {
   return (
-    <div className='loading'>
+    <div className="loading">
       <h4>Loading...</h4>
-      <img className='loadingSpinner' src={loadingGif} alt='' />
     </div>
   );
 };
